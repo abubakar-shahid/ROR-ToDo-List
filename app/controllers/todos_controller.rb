8a6636e5abec3c473f86ef8395b7992c084cdb60
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-08-24 20:00:48 +0500

    Added controllers code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,26 @@
+# app/controllers/todos_controller.rb
 class TodosController < ApplicationController
   def index
+    response = $firebase.get("todos")
+    @todos = response.body || {}
   end
 
   def create
+    todo = { title: params[:title], completed: false }
+    response = $firebase.push("todos", todo)
+    redirect_to root_path
   end
 
   def update
+    todo_id = params[:id]
+    updated_todo = { title: params[:title], completed: params[:completed] }
+    response = $firebase.update("todos/#{todo_id}", updated_todo)
+    redirect_to root_path
   end
 
   def destroy
+    todo_id = params[:id]
+    response = $firebase.delete("todos/#{todo_id}")
+    redirect_to root_path
   end
 end
