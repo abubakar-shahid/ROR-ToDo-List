52a823d24423db514e8443a7c8bc963e6dc8ddab
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-08-26 22:52:13 +0500

    new route added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'todos#index'
   resources :todos, only: [:index, :create, :update, :destroy]
   get 'todos/render_todo_partial', to: 'todos#render_todo_partial'
+  post 'todos/save_from_firebase', to: 'todos#save_from_firebase'
 end
