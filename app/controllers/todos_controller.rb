b335eebb80669496ff537040d74708728cb6c5de
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-08-26 21:09:47 +0500

    code running but with the main page reloading thing

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,64 +1,38 @@
 # app/controllers/todos_controller.rb
 class TodosController < ApplicationController
-  before_action :set_firebase, only: [:index, :create, :update, :destroy]
 
   def index
-    response = @firebase.get("todos")
-    if response.success?
-      @todos = response.body || {}
-    else
-      @todos = {}
-      Rails.logger.error "Failed to retrieve todos from Firebase: #{response.code} - #{response.body}"
-    end
+    response = $firebase.get("todos")
+    @todos = response.body || {}
   end
 
   def create
     Rails.logger.info "in create function"
     todo = { title: params[:title], completed: false }
-    response = @firebase.push("todos", todo)
-    if response.success?
-      redirect_to root_path, notice: "Todo was successfully created."
-    else
-      redirect_to root_path, alert: "Failed to create todo: #{response.code} - #{response.body}"
-    end
+    response = $firebase.push("todos", todo)
+    redirect_to root_path
   end
 
   def update
     todo_id = params[:id]
     updated_todo = { title: params[:title], completed: params[:completed] }
-    response = @firebase.update("todos/#{todo_id}", updated_todo)
-    if response.success?
-      redirect_to root_path, notice: "Todo was successfully updated."
-    else
-      redirect_to root_path, alert: "Failed to update todo: #{response.code} - #{response.body}"
-    end
+    response = $firebase.update("todos/#{todo_id}", updated_todo)
+    redirect_to root_path
   end
 
   def destroy
     Rails.logger.info "in destroy function"
     todo_id = params[:id]
-    response = @firebase.delete("todos/#{todo_id}")
-    if response.success?
-      redirect_to root_path, notice: "Todo was successfully deleted."
-    else
-      redirect_to root_path, alert: "Failed to delete todo: #{response.code} - #{response.body}"
-    end
+    response = $firebase.delete("todos/#{todo_id}")
+    redirect_to root_path
   end
 
   def render_todo_partial
     @id = params[:id]
-    @todo = {
-      title: params[:todo][:title],
-      completed: params[:todo][:completed] == 'true'
-    }
+    @todo = params[:todo]
     render partial: 'todo', locals: { id: @id, todo: @todo }
   end
-
-  private
-
-  def set_firebase
-    @firebase = $firebase
-  end
+  
 end
 
 
@@ -70,7 +44,52 @@ end
 
 
 
-# # app/controllers/todos_controller.rb
+
+# class TodosController < ApplicationController
+#   def index
+#     response = $firebase.get("todos")
+#     @todos = response.body || {}
+#   end
+
+#   def create
+#     Rails.logger.info "in create function"
+#     todo = { title: params[:title], completed: false }
+#     response = $firebase.push("todos", todo)
+
+#     render partial: 'todo', locals: { id: response.body['name'], todo: todo }
+#   end
+
+#   def update
+#     todo_id = params[:id]
+#     updated_todo = { title: params[:title], completed: params[:completed] }
+#     response = $firebase.update("todos/#{todo_id}", updated_todo)
+
+#     render partial: 'todo', locals: { id: todo_id, todo: updated_todo }
+#   end
+
+#   def destroy
+#     Rails.logger.info "in destroy function"
+#     todo_id = params[:id]
+#     response = $firebase.delete("todos/#{todo_id}")
+
+#     render js: "$('#todo-#{todo_id}').remove();"
+#   end
+
+#   def render_todo_partial
+#     @id = params[:id]
+#     @todo = params[:todo]
+#     render partial: 'todo', locals: { id: @id, todo: @todo }
+#   end
+# end
+
+
+
+
+
+
+
+
+
 # class TodosController < ApplicationController
 
 #   def index
@@ -82,27 +101,39 @@ end
 #     Rails.logger.info "in create function"
 #     todo = { title: params[:title], completed: false }
 #     response = $firebase.push("todos", todo)
-#     redirect_to root_path
+
+#     respond_to do |format|
+#       format.html { redirect_to root_path }
+#       format.js   { render partial: 'todo', locals: { id: response.body['name'], todo: todo } }
+#     end
 #   end
 
 #   def update
 #     todo_id = params[:id]
 #     updated_todo = { title: params[:title], completed: params[:completed] }
 #     response = $firebase.update("todos/#{todo_id}", updated_todo)
-#     redirect_to root_path
+
+#     respond_to do |format|
+#       format.html { redirect_to root_path }
+#       format.js   { render partial: 'todo', locals: { id: todo_id, todo: updated_todo } }
+#     end
 #   end
 
 #   def destroy
 #     Rails.logger.info "in destroy function"
 #     todo_id = params[:id]
 #     response = $firebase.delete("todos/#{todo_id}")
-#     redirect_to root_path
+
+#     respond_to do |format|
+#       format.html { redirect_to root_path }
+#       format.js   { render js: "$('#todo-#{todo_id}').remove();" }
+#     end
 #   end
 
-#   def render_todo_partial
+#     def render_todo_partial
 #     @id = params[:id]
 #     @todo = params[:todo]
 #     render partial: 'todo', locals: { id: @id, todo: @todo }
 #   end
-  
+
 # end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,48 +5,52 @@
 
 
 // document.addEventListener('turbo:load', () => {
-    // Your Firebase code here remains the same
-    const firebaseConfig = {
-        apiKey: "your-api-key",
-        authDomain: "your-auth-domain",
-        databaseURL: "your-database-url",
-        projectId: "your-project-id",
-        storageBucket: "your-storage-bucket",
-        messagingSenderId: "your-messaging-sender-id",
-        appId: "your-app-id",
-        measurementId: "your-measurement-id"
-    };
+// document.addEventListener('DOMContentLoaded', function () {
+const firebaseConfig = {
+    apiKey: "AIzaSyAqCDQ3IsupyhP0uKr90bou3MAbfY3ooiM",
+    authDomain: "concepthealth-7b3a9.firebaseapp.com",
+    databaseURL: "https://concepthealth-7b3a9-default-rtdb.firebaseio.com",
+    projectId: "concepthealth-7b3a9",
+    storageBucket: "concepthealth-7b3a9.appspot.com",
+    messagingSenderId: "132826073965",
+    appId: "1:132826073965:web:d69e4e8ee1737cd2a3a864",
+    measurementId: "G-YDJZV624VH"
+};
 
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.database();
-    const todosRef = db.ref('todos');
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const todosRef = db.ref('todos');
 
-    todosRef.on('value', (snapshot) => {
-        const todos = snapshot.val();
-        updateTodos(todos);
-    });
+let x = 0;
 
-    function updateTodos(todos) {
-        for (const [id, todo] of Object.entries(todos)) {
-            fetch(`/todos/render_todo_partial?id=${id}&todo[title]=${encodeURIComponent(todo.title)}&todo[completed]=${todo.completed}`, {
-                method: 'GET',
-                headers: {
-                    'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+todosRef.on('value', (snapshot) => {
+    x++;
+    console.log("updated recieved: ", x);
+    const todos = snapshot.val();
+    updateTodos(todos);
+});
+
+function updateTodos(todos) {
+    for (const [id, todo] of Object.entries(todos)) {
+        fetch(`/todos/render_todo_partial?id=${id}&todo[title]=${encodeURIComponent(todo.title)}&todo[completed]=${todo.completed}`, {
+            method: 'GET',
+            headers: {
+                'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            }
+        })
+            .then(response => response.text())
+            .then(html => {
+                const li = document.getElementById(`todo-${id}`);
+                if (li) {
+                    li.outerHTML = html; // Replace existing item
+                } else {
+                    const todoList = document.querySelector('ul');
+                    todoList.insertAdjacentHTML('beforeend', html); // Add new item
                 }
             })
-                .then(response => response.text())
-                .then(html => {
-                    const li = document.getElementById(`todo-${id}`);
-                    if (li) {
-                        li.outerHTML = html; // Replace existing item
-                    } else {
-                        const todoList = document.querySelector('ul');
-                        todoList.insertAdjacentHTML('beforeend', html); // Add new item
-                    }
-                })
-                .catch(error => console.error('Error fetching the todo partial:', error));
-        }
+            .catch(error => console.error('Error fetching the todo partial:', error));
     }
+}
 // });
 
 
